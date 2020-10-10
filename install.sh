3a35115a7cd5a309aa197bd20b57d0241ac0dfe2
sonphantrung <phantrungson17@gmail.com>
2020-10-10 18:39:23 +0700

    Edit

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 git clone --bare --recursive git@gitlab.org:phantrungson17/dots.git
 git clone git@gitlab.org:phantrungson17/st-phantrungson.git
 git clone git@gitlab.org:phantrungson17/dmenu-phantrungson.git
-git clone git@gitlab.org:phantrungson17/Slstatus.git
+git clone git@gitlab.org:phantrungson17/Slstaus.git
 git clone git@gitlab.org:phantrungson17/dwm-phantrungson.git
 function config {
    /usr/bin/git --git-dir=$HOME/dots/ --work-tree=$HOME $@
@@ -12,7 +12,7 @@ if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no

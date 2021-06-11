f399b6795dd217478245a97c41ccdc89ac671aa9
Phan Trung Son <phantrungson17@gmail.com>
2021-06-11 10:16:47 +0700

    Some edits

--- .local/bin/rec-nosound.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-ffmpeg -f x11grab -video_size 1366x768 -framerate 25 -i $DISPLAY -c:v libx264 ~/Videos/screen-$(date +%Y.%m.%d-%H.%M.%S).mp4
--- .local/bin/rec.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-ffmpeg -f x11grab -video_size 1366x768 -framerate 30 -i $DISPLAY -f alsa -i default -c:v libx264 -preset ultrafast -c:a aac ~/Videos/screen-$(date +%Y.%m.%d-%H.%M.%S).mp4

0d988cdfb6fe8fc41f93260541306f2408aba941
Son Phan Trung <phantrungson17@gmail.com>
2021-10-02 14:18:11 +0700

    Backing up

--- .config/nvim/lua/lsp/configure.lua
+++ .config/nvim/lua/lsp/configure.lua
@@ -1,6 +1,7 @@
-local signs = { Error = "", Warning = "", Hint = "", Information = "" }
+local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
 
 for type, icon in pairs(signs) do
   local hl = "LspDiagnosticsSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
+
